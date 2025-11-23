95e5131be34729ff2418cbaf2b9bcda30c21a5fd
Mikolaj <mikolaj.kubista@gmail.com>
2025-11-23 12:31:05 +0100

    custom converter for date only serialization

---
+++ AssigmentApp/CustomConverters/DateOnlyJsonConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AssigmentApp.CustomConverters;
+
+public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        DateOnly.ParseExact(reader.GetString()!, "yyyyMMdd", CultureInfo.InvariantCulture);
+    
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) =>
+        writer.WriteStringValue(value.ToString("yyyyMMdd"));
+}
