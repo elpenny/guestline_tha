5f43d68c03caef007c9126b6a593b5cc148df1e0
Mikołaj Kubista <mikolaj.kubista@gmail.com>
2025-11-24 20:40:31 +0100

    Command Parser tests

--- AssigmentApp/Parsing/CommandParser.cs
+++ AssigmentApp/Parsing/CommandParser.cs
@@ -1,6 +1,9 @@
-﻿namespace AssigmentApp.Parsing;
+﻿using FluentResults;
+
+namespace AssigmentApp.Parsing;
 
 public static class CommandParser
 {
-    
+    public static Result<T> Parse<T>(string input) where T : struct =>
+        throw new NotImplementedException();
 }
