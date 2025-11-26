f2db6d19475862430b0f8c794a60e347d011b170
Mikołaj Kubista <mikolaj.kubista@gmail.com>
2025-11-26 11:22:28 +0100

    fixes

--- AssigmentApp/IO/DateOnlyJsonConverter.cs
+++ AssigmentApp/IO/DateOnlyJsonConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using AssigmentApp.Parsing;
 
@@ -6,8 +7,19 @@ namespace AssigmentApp.IO;
 
 public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
 {
-    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-        BookingDateParser.ParseDate(reader.GetString()!);
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var raw = reader.GetString();
+        if (string.IsNullOrWhiteSpace(raw))
+            throw new JsonException("Date value is empty.");
+
+        var parsed = BookingDateParser.ParseDateResult(raw);
+        if (parsed.IsFailed)
+            throw new JsonException(parsed.Errors.First().Message);
+
+        return parsed.Value;
+    }
+
     public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) =>
         writer.WriteStringValue(value.ToString("yyyyMMdd"));
 }
